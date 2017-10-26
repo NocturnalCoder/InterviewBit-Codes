0b71ba820a163806c3f42f932b5081dbeb69da71
NocturnalCoder <prateekkp96@gmail.com>
2017-10-27 01:10:54 +0530

    Update Max Rectangle in Binary Matrix.java

--- Max Rectangle in Binary Matrix.java
+++ Max Rectangle in Binary Matrix.java
@@ -1,5 +1,5 @@
 /*
-Max Rectangle in Binary Matrix
+Max Rectangle in Binary Matrix 
 Given a 2D binary matrix filled with 0’s and 1’s, find the largest rectangle containing all ones and return its area.
 
 Bonus if you can solve it in O(n^2) or less.
